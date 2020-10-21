440975c599c2d8e42d726f199f9cd5d980c61afe
Heather1337 <heatherlynn1337@gmail.com>
2020-10-21 16:12:36 -0700

    Setup button click handler.

--- static/js/coffeeShop.js
+++ static/js/coffeeShop.js
@@ -38,3 +38,8 @@ const setProgressAndStatus = (progressVal, statusMsg) => {
 //
 // Add your event handlers below.
 //
+$('.add-to-order').on('click', ()=> {
+  addItemToCart('Coffee');
+  incrementCartTotal(1.50);
+});
+
